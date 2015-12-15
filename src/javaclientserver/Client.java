cf685e13469d396e62865119e6723eb618056494
mnsekh111 <mnsekh111@gmail.com>
2015-12-15 04:28:03 -0500

    added echo client

--- src/javaclientserver/Client.java
+++ src/javaclientserver/Client.java
@@ -5,10 +5,72 @@
  */
 package javaclientserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mns
  */
 public class Client {
-    
+
+    public static void main(String[] args) {
+        String serverHostname = new String("127.0.0.1");
+        int portNum = 9991;
+
+        if (args.length > 0) {
+            serverHostname = args[0];
+        }
+        System.out.println("Attemping to connect to host "
+                + serverHostname + " on port 10007.");
+
+        Socket socket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+
+        try {
+
+            socket = new Socket(serverHostname, portNum);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.err.println("Unknown host " + serverHostname);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't connect to " + serverHostname);
+            System.exit(1);
+        }
+
+        BufferedReader stdIn = new BufferedReader(
+                new InputStreamReader(System.in));
+        String userInput;
+
+        System.out.print("Enter a message : ");
+        try {
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+                System.out.print("Enter a message : ");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+
+            out.close();
+            in.close();
+            stdIn.close();
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
