e2ccccfecdeaef5e5646b54f6fe54c8a7f2d89b1
mnsekh111 <mnsekh111@gmail.com>
2015-12-15 04:19:00 -0500

    Added echo server

--- src/javaclientserver/Server.java
+++ src/javaclientserver/Server.java
@@ -5,10 +5,69 @@
  */
 package javaclientserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mns
  */
 public class Server {
-    
+
+    public static void main(String[] args) {
+        int portNum = 9991;
+
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(portNum);
+            System.out.println("Listening on port " + portNum);
+        } catch (IOException ie) {
+            System.err.println("Error listening at port " + portNum + "\n" + ie.getLocalizedMessage());
+            System.exit(1);
+        }
+
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+
+        try {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
+                    true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Server: " + inputLine);
+                out.println(inputLine);
+
+                if (inputLine.equals("Bye.")) {
+                    break;
+                }
+            }
+
+            out.close();
+            in.close();
+            clientSocket.close();
+            serverSocket.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
 }
